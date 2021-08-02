bbbc17292014a7c776e63b4c33e9ff9492716ff9
pablojoka <pablojoka@hotmail.com>
2021-08-02 15:51:45 -0300

    Cargados los 2 archivos

---
+++ autos.js
@@ -0,0 +1,36 @@
+let autos = [
+    {
+        marca:"Ford",
+        modelo:"Fiesta",
+        color:"Azul",
+        anio:2019,
+        km:200,
+        precio:150000,
+        cuotas:12,
+        patente:"APL123",
+        vendido:true,
+    },
+     {
+        marca:"Toyota",
+        modelo:"Corolla",
+        color:"Blanco",
+        anio:2019,
+        km:0,
+        precio:100000,
+        cuotas:14,
+        patente:"JJK116",
+        vendido:false,
+    },
+    {
+        marca:"Suzuki",
+        modelo:"Fun",
+        color:"Rojo",
+        anio:2010,
+        km:120.000,
+        precio:50000,
+        cuotas:14,
+        patente:"HZH882",
+        vendido:true,
+    }
+]
+module.exports = autos;
---
+++ concesionaria.js
@@ -0,0 +1,94 @@
+const autos = require('./autos.js');
+
+
+const concesionaria = {
+    autos: autos,
+    buscarAuto: function (patente) {
+        let autoEncontrado = autos.filter(elemento => elemento.patente == patente);
+        if (autoEncontrado != "") {
+            return autoEncontrado[0];
+        } else {
+            return null;
+        }
+
+    },
+    venderAuto: function (patente) {
+        let autoVendido = this.buscarAuto(patente);
+        autoVendido.vendido = true;
+        return autoVendido;
+    },
+    autosParaLaVenta: function () {
+        let autosALaVenta = autos.filter(elemento => elemento.vendido == false);
+        return autosALaVenta;
+    },
+    autosNuevos: function () {
+        let autosEnVenta = this.autosParaLaVenta();
+        let autos0Km = autosEnVenta.filter(elemento => elemento.km < 100);
+        return autos0Km;
+    },
+    listaDeVentas: function (patente) {
+        let listaDePrecios = [];
+        let autosYaVendidos = autos.filter(elemento => elemento.vendido == true);
+        for (let i = 0; i < autosYaVendidos.length; i++) {
+            let precioDeAuto = autosYaVendidos[i].precio;
+            listaDePrecios.push(precioDeAuto);
+        }
+        let autoVendido = this.venderAuto(patente);
+        listaDePrecios.push(autoVendido.precio);
+
+        return listaDePrecios;
+        
+        
+    }
+
+
+    /*Al llamar a la función listaDeVentas debe retornar una lista con los precios de las ventas, la lista original debe devolver una lista vacia
+Al vender al auto con patente 'JJK116' y luego llamar a la función listaDeVentas debe contener el precio del auto vendido */
+    
+
+/*listaDeVentas: function (patente) {
+        let listaDePrecios = [];
+        let autosYaVendidos = autos.filter(elemento => elemento.vendido == true);
+        for (let i = 0; i < autosYaVendidos.length; i++) {
+            let precioDeAuto = autosYaVendidos[i].precio;
+            listaDePrecios.push(precioDeAuto);
+        }
+        let autoVendido = this.venderAuto(patente);
+        listaDePrecios.push(autoVendido.precio);
+
+        return listaDePrecios;
+        
+        
+    }
+*/
+  
+}
+//console.log(concesionaria.venderAuto('APL123'));
+console.log(concesionaria.listaDeVentas('APL123'));
+
+
+
+
+
+
+
+    /*listaDeVenta: function () {
+        let autosVendidos = autos.filter(elemento => elemento.vendido == true);
+        let listaDePrecios = [];
+        for (let i = 0; i < autosVendidos.length; i++) {
+            let precioDeAuto = autosVendidos[i].precio;
+            listaDePrecios.push(precioDeAuto);
+        }
+        return listaDePrecios;
+    }*/
+
+
+
+
+
+  
+
+//console.log(concesionaria.buscarAuto('APL123'))
+//concesionaria.venderAuto('APL123');
+
+
